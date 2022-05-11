8db8b7b83133e97a795ad21bf17768c2dc50fdfd
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-05-11 10:22:17 +0900

    feat: get path by args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
-    let path = "./src/main.rs";
-    match std::fs::read_to_string(path) {
-        Ok(content) => print!("{}", content),
-        Err(why) => println!("{}", why)        
+
+    let mut args = std::env::args();
+
+    match args.nth(1) {
+        Some(path) => match std::fs::read_to_string(path) {
+            Ok(content) => print!("{}", content),
+            Err(why) => println!("{}", why)
+        },
+        None => println!("1つ目の実行時引数にファイルパスを入れる必要があります。")
     }
 }
