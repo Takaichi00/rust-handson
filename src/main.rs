3b9546756adec677837d9c90e7e14a377aaef1b2
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-05-11 10:24:39 +0900

    refactor: run method

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,14 @@ fn main() {
     let mut args = std::env::args();
 
     match args.nth(1) {
-        Some(path) => match std::fs::read_to_string(path) {
-            Ok(content) => print!("{}", content),
-            Err(why) => println!("{}", why)
-        },
+        Some(path) => run(path),
         None => println!("1つ目の実行時引数にファイルパスを入れる必要があります。")
+    }
+}
+
+fn run(path: String) {
+    match std::fs::read_to_string(path) {
+        Ok(content) => print!("{}", content),
+        Err(why) => println!("{}", why)
     }
 }
