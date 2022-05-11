fa25c9733bd9d2b6806b97acae33e4976cdb5354
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-05-11 10:16:01 +0900

    feat: read to string main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let path = "./src/main.rs";
+    match std::fs::read_to_string(path) {
+        Ok(content) => print!("{}", content),
+        Err(why) => println!("{}", why)        
+    }
 }
